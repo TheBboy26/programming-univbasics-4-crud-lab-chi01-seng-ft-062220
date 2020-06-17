6303de9a44771329feaccdf45f6abca3b2e3fe06
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-17 06:38:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,5 +46,6 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-
+  update_element = ['one','two','arrays!']
+  update_element[2]
 end
