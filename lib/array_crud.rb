b31737c0d03051e6bdd8e6758e8b4db2f88d0011
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-17 06:23:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
+  add_end = ['water', 'soda', 'juice']
   
+  add_end.shift
 end
 
 def add_element_to_start_of_array(array, element)
