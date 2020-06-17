391b9c7d26b76ade750ef25fb12ee94120434a89
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-17 06:31:00 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -22,7 +22,7 @@ end
 def remove_element_from_end_of_array(array)
   remove_end = ['codes', 'binary', 'arrays!']
   
-  arrays!_gone = 
+  arrays!_gone = remove_end.pop
 end
 
 def remove_element_from_start_of_array(array)
