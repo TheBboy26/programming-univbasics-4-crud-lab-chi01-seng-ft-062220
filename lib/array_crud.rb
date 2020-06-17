e1fc98b90049675a3e7ba3613a7293c3d4a02a9d
Josue Viramontes <josueviramontes1997@gmail.com>
2020-06-17 06:37:16 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -41,7 +41,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  final_element = ['one','two','arrays!']
+  final_element[2]
 end
 
 def update_element_from_index(array, index_number, element)
